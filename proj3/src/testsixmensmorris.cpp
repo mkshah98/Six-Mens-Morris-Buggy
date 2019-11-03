16bfe65a7339e5b34fc4f85025da3518bdf817f3
mkshah98 <46585928+mkshah98@users.noreply.github.com>
2019-11-03 12:10:09 -0800

    Update testsixmensmorris.cpp
    All tests

--- proj3/src/testsixmensmorris.cpp
+++ proj3/src/testsixmensmorris.cpp
@@ -12,7 +12,7 @@ TEST(SixMensMorrisBoardTest, DefaultBoardTest){
     EXPECT_EQ(EmptyBoard.UnplacedPieces(SIX_MENS_MORRIS_PLAYER_W), SIX_MENS_MORRIS_PIECES);
     EXPECT_FALSE(EmptyBoard.GameOver());
     EXPECT_EQ(EmptyBoard.ToString(),">RU:6 RC:0  WU:6 WC:0\n" 
-                                    "o---------o---------o      0---1---2\n" 
+                                    "o---------o---------o u     0---1---2\n" 
                                     "|         |         |      | 3-4-5 |\n"
                                     "|         |         |      6-7   8-9\n"
                                     "|    o----o----o    |      | A-B-C |\n"
@@ -67,7 +67,6 @@ TEST(SixMensMorrisBoardTest, SetBoardTest){
                                     "o---------o---------R\n");
     EXPECT_EQ(board.operator std::string(),board.ToString());
 }
-
 
 TEST(SixMensMorrisBoardTest, ResetBoardTest){
 	char turn = SIX_MENS_MORRIS_PLAYER_W;
@@ -194,25 +193,24 @@ TEST(SixMensMorrisBoardTest, PlacementMillTest){
                                     "o---------o---------o\n");
 }
 
-TEST(SixMensMorrisBoardTest, MoveTest){ 
-    CSixMensMorrisBoard board;
-    
+TEST(SixMensMorrisBoardTest, MoveTest){
     char turn = SIX_MENS_MORRIS_PLAYER_R;
     int unplaced[SIX_MENS_MORRIS_PLAYERS] = {0,0}; //all pieces on the board
-    // Needs to test that movement is correct
-
-    char turn = SIX_MENS_MORRIS_PLAYER_R;
-    int unplaced[SIX_MENS_MORRIS_PLAYERS] = {3,3};
-    //pull up any board, test moving from one positon to another
     const char positions[SIX_MENS_MORRIS_POSITIONS] = {SIX_MENS_MORRIS_PLAYER_R,SIX_MENS_MORRIS_PLAYER_W,SIX_MENS_MORRIS_EMPTY,
                                                         SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_PLAYER_R,
                                                         SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_PLAYER_W,
                                                         SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_PLAYER_W,SIX_MENS_MORRIS_EMPTY,
                                                         SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_PLAYER_R};
     
+    const char previous[SIX_MENS_MORRIS_POSITIONS] = {SIX_MENS_MORRIS_PLAYER_R,SIX_MENS_MORRIS_PLAYER_W,SIX_MENS_MORRIS_PLAYER_W,
+                                                        SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_PLAYER_R,
+                                                        SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_EMPTY,
+                                                        SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_PLAYER_W,SIX_MENS_MORRIS_EMPTY,
+                                                        SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_PLAYER_R};
 
+    CSixMensMorrisBoard board(turn, unplaced, positions, previous);
     /*
-                                                                                                " RU:2 RC:1 >WU:3 WC:0\n" 
+                                                                            " RU:2 RC:1 >WU:3 WC:0\n" 
                                                                             "R---------W---------O      0---1---2\n" 
                                                                             "|         |         |      | 3-4-5 |\n"
                                                                             "|         |         |      6-7   8-9\n"
@@ -231,16 +229,16 @@ TEST(SixMensMorrisBoardTest, MoveTest){
     //canmove, then move
 
     //check player at the position, expect player u just moved. R 11 -> 12, call player @ pos 12, = r.
-    EXPECT_TRUE(board.PlayeratPosition(0) = ‘R’); //see what player is at pos 0
+    EXPECT_EQ(board.PlayeratPosition(0), SIX_MENS_MORRIS_PLAYER_R); //see what player is at pos 0
     EXPECT_TRUE(board.CanMove(SIX_MENS_MORRIS_PLAYER_R,0)); // ^if that went through, then we will expect true if we can move it one down.
     EXPECT_TRUE(board.Move(SIX_MENS_MORRIS_PLAYER_R,0,6)); //move it to pos 6
     
     //move R (red turn), expect next turn = W
-    EXPECT_TRUE(board.PlayerTurn == 'W');
+    EXPECT_EQ(board.PlayerTurn(), SIX_MENS_MORRIS_PLAYER_W);
 
     //checking if you can move a white piece
     EXPECT_TRUE(board.CanMove(SIX_MENS_MORRIS_PLAYER_W,8));
-    EXPECT_TRUE(board.Move(SIX_MENS_MORRIS_PLAYER_W,9,8)) //move from W from 9 to 8. shift left
+    EXPECT_TRUE(board.Move(SIX_MENS_MORRIS_PLAYER_W,9,8)); //move from W from 9 to 8. shift left
 
 
     //see if you can move distant place movig 3 -> 15 b/c u cant fly so it should be expect_false.
@@ -270,36 +268,94 @@ TEST(SixMensMorrisBoardTest, MoveTest){
                                     "|         |         |\n"
                                     "o---------o---------R\n");
 }
+}
 
 TEST(SixMensMorrisBoardTest, MoveMillTest){
-    // Needs to test that movement creating a mill is correct with removal
-    
+    char player = SIX_MENS_MORRIS_PLAYER_R;
+    const int unplaced[SIX_MENS_MORRIS_PLAYERS] = {0,0};
+    const char positions[SIX_MENS_MORRIS_POSITIONS] = {SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_PLAYER_R,SIX_MENS_MORRIS_PLAYER_R,
+                                                        SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_PLAYER_W,SIX_MENS_MORRIS_PLAYER_R,
+                                                        SIX_MENS_MORRIS_PLAYER_R,SIX_MENS_MORRIS_PLAYER_W,SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_EMPTY,
+                                                        SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_PLAYER_W,SIX_MENS_MORRIS_PLAYER_W,
+                                                        SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_PLAYER_W};
+
+    const char previous[SIX_MENS_MORRIS_POSITIONS] = {SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_PLAYER_R,SIX_MENS_MORRIS_PLAYER_R,
+                                                        SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_PLAYER_W,SIX_MENS_MORRIS_PLAYER_R,
+                                                        SIX_MENS_MORRIS_PLAYER_R,SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_PLAYER_W,SIX_MENS_MORRIS_EMPTY,
+                                                        SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_PLAYER_W,SIX_MENS_MORRIS_PLAYER_W,
+                                                        SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_PLAYER_W};
+    CSixMensMorrisBoard board(player, unplaced, positions, previous);
+    EXPECT_TRUE(board.CanMove(player, 0));
+    EXPECT_TRUE(board.Move(player, 6, 0));
+    EXPECT_TRUE(board.CanRemove(player));
+    EXPECT_TRUE(board.Remove(SIX_MENS_MORRIS_PLAYER_W, 15));
+    EXPECT_EQ(board.ToString(), " RU:0 RC:3 >WU:0 WC:2\n" 
+                                    "R---------R---------R      0---1---2\n" 
+                                    "|         |         |      | 3-4-5 |\n"
+                                    "|         |         |      6-7   8-9\n"
+                                    "|    o----W----R    |      | A-B-C |\n"
+                                    "|    |         |    |      D---E---F\n"
+                                    "|    |         |    |        LEGEND\n"
+                                    "o----W         o----o\n"
+                                    "|    |         |    |\n"
+                                    "|    |         |    |\n"
+                                    "|    o----o----W    |\n"
+                                    "|         |         |\n"
+                                    "|         |         |\n"
+                                    "o---------o---------o\n");
 }
                                        
 TEST(SixMensMorrisBoardTest, TwoPieceGameOverTest){
-    CSixMensMorrisBoard board;
-    EXPECT_TRUE(board.GameOver());
-    CSixMensMorrisBoard board2;
-    EXPECT_FALSE(board2.GameOver());
-    // Needs to test that game over is correct when only two pieces left
+    char player = SIX_MENS_MORRIS_PLAYER_W;
+    const int unplaced[SIX_MENS_MORRIS_PLAYERS] = {0,0};
+    const char positions[SIX_MENS_MORRIS_POSITIONS] = {SIX_MENS_MORRIS_PLAYER_R,SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_PLAYER_R,
+                                                        SIX_MENS_MORRIS_PLAYER_W,SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_EMPTY,
+                                                        SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_PLAYER_W,SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_EMPTY,
+                                                        SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_PLAYER_W,SIX_MENS_MORRIS_EMPTY,
+                                                        SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_PLAYER_R};
 
-    //unplaced would be (0,0) 
-    int unplaced[SIX_MENS_MORRIS_PLAYERS] = {0,0};
-    //once i create the board, move through each function to see if its true or not
-    //canmove, then move, create a mill.once mill created then u can remove.
+    const char previous[SIX_MENS_MORRIS_POSITIONS] = {SIX_MENS_MORRIS_PLAYER_R,SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_EMPTY,
+                                                        SIX_MENS_MORRIS_PLAYER_W,SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_EMPTY,
+                                                        SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_PLAYER_W,SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_PLAYER_R,
+                                                        SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_PLAYER_W,SIX_MENS_MORRIS_EMPTY,
+                                                        SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_PLAYER_R};
+    CSixMensMorrisBoard board(player, unplaced, positions, previous);
+    EXPECT_TRUE(board.CanMove(SIX_MENS_MORRIS_PLAYER_W, 11));
+    EXPECT_TRUE(board.Move(SIX_MENS_MORRIS_PLAYER_W,12,11));
+    EXPECT_TRUE(board.CanRemove(SIX_MENS_MORRIS_PLAYER_W));
+    EXPECT_TRUE(board.Remove(SIX_MENS_MORRIS_PLAYER_R,SIX_MENS_MORRIS_POSITIONS-1));
+    EXPECT_TRUE(board.GameOver());
+    EXPECT_EQ(board.ToString(),
+                                    " RU:0 RC:3  WU:0 WC:4\n" 
+                                    "R---------o---------R      0---1---2\n" 
+                                    "|         |         |      | 3-4-5 |\n"
+                                    "|         |         |      6-7   8-9\n"
+                                    "|    W----o----o    |      | A-B-C |\n"
+                                    "|    |         |    |      D---E---F\n"
+                                    "|    |         |    |        LEGEND\n"
+                                    "o----W         o----o\n"
+                                    "|    |         |    |\n"
+                                    "|    |         |    |\n"
+                                    "|    W----o----o    |\n"
+                                    "|         |         |\n"
+                                    "|         |         |\n"
+                                    "o---------o---------o\n");
 }
 
 TEST(SixMensMorrisBoardTest, NoMoveGameOverTest){
-    // Needs to test that game over is correct when no movement allowed
-    CSixMensMorrisBoard board;
+    const int unplaced[SIX_MENS_MORRIS_PLAYERS] = {0,0};
     char turn = SIX_MENS_MORRIS_PLAYER_W;
-    //WHITE'S turn b/c white went first.
     const char positions[SIX_MENS_MORRIS_POSITIONS] = {SIX_MENS_MORRIS_PLAYER_W,SIX_MENS_MORRIS_PLAYER_R,SIX_MENS_MORRIS_PLAYER_R,
                                                         SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_PLAYER_W,SIX_MENS_MORRIS_EMPTY,
                                                         SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_PLAYER_W,SIX_MENS_MORRIS_PLAYER_R,
                                                         SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_EMPTY,
                                                         SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_PLAYER_W,SIX_MENS_MORRIS_EMPTY};
-    
+    const char previous[SIX_MENS_MORRIS_POSITIONS] = {SIX_MENS_MORRIS_PLAYER_W,SIX_MENS_MORRIS_PLAYER_R,SIX_MENS_MORRIS_PLAYER_R,
+                                                        SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_PLAYER_W,SIX_MENS_MORRIS_EMPTY,
+                                                        SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_PLAYER_W,SIX_MENS_MORRIS_EMPTY,
+                                                        SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_EMPTY,
+                                                        SIX_MENS_MORRIS_EMPTY,SIX_MENS_MORRIS_PLAYER_W,SIX_MENS_MORRIS_PLAYER_R};
+    CSixMensMorrisBoard board(turn, unplaced, positions, previous);
     EXPECT_TRUE(board.CanMove(SIX_MENS_MORRIS_PLAYER_W,15));
     EXPECT_TRUE(board.Move(SIX_MENS_MORRIS_PLAYER_W,14,15)); // on board 15 to 16
     
@@ -308,15 +364,9 @@ TEST(SixMensMorrisBoardTest, NoMoveGameOverTest){
     EXPECT_FALSE(board.CanMove(SIX_MENS_MORRIS_PLAYER_R,9));
 
     EXPECT_FALSE(board.CanMove(SIX_MENS_MORRIS_PLAYER_R,1));
-    EXPECT_TRUE(board.GameOver);
-
-
-    //do a canmove(14 to 15) (index 0) actually 16.
-    //move W to 15 and then EXPECT_TRUE(board.gameover)
-    //previous is basically showing red's turn, because it is one behind.
-    //now dark can't move as shown below, R is blocked by all the white pieces.
+    EXPECT_TRUE(board.GameOver());
     EXPECT_EQ(board.ToString(),
-                                    " RU:3 RC:3 WU:3 WC:2\n" 
+                                    " RU:0 RC:3 WU:0 WC:3\n" 
                                     "W---------R---------R      0---1---2\n" 
                                     "|         |         |      | 3-4-5 |\n"
                                     "|         |         |      6-7   8-9\n"
@@ -330,13 +380,6 @@ TEST(SixMensMorrisBoardTest, NoMoveGameOverTest){
                                     "|         |         |\n"
                                     "|         |         |\n"
                                     "o---------o---------W\n");
-                                    //remove the RC:3 > WU:3 to RC:3 WU:3 b/c the game is over
-                                    //removing indicator indicicationg whose turn it is.
-                                    //OG >RC:3 WU:3.
-    //once i create the board, move through each function to see if its true or not
-    //canmove, then move, create a mill.once mill created then u can remove.
-
-
 }
 
 TEST(SixMensMorrisBoardTest, BadParametersTest){
